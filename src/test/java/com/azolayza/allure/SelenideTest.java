f5e40f4259a759abdec656db656ffc82d0859804
azolayza <azovcevae@gmail.com>
2022-01-13 16:34:36 +0700

    fix selenideTest with listener
    add imports for SelenideLogger.addListener

--- src/test/java/com/azolayza/allure/SelenideTest.java
+++ src/test/java/com/azolayza/allure/SelenideTest.java
@@ -2,6 +2,7 @@ package com.azolayza.allure;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
